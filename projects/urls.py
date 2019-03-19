b8e74fe903be401b962ec3df719a43028cafcdf4
walidamer711 <walidamer711@gmail.com>
2019-03-18 23:26:37 -0700

    first commit

---
+++ projects/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('list', views.ProjectList.as_view(), name='projects'),
+    path('newproject', views.ProjectCreate.as_view(), name='new_project'),
+    path('project/<int:project_id>', views.TaskList.as_view(), name='project'),
+    path('project/<int:project_id>/newtask', views.TaskCreate.as_view(), name='new_task'),
+    path('project/<int:project_id>/task/<int:task_id>', views.TaskUpdate.as_view(), name='task_update'),
+    path('project/<int:project_id>/task/<int:task_id>/delete', views.TaskDelete.as_view(), name='task_delete'),
+]
